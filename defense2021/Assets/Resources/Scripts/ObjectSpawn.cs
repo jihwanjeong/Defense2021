b7cd3820170887f525539edde41cc9d3cc9bc3b9
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-07-24 20:52:07 +0900

    spawn 임시구현

---
+++ defense2021/Assets/Resources/Scripts/ObjectSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    public bool enableSpawn = false;
+    public int round = 1;
+    public float row = 0.5f;
+    public Text roundsinfo;
+    public GameObject Enemy; 
+    void SpawnEnemy()
+    {
+        setRoundinfo();
+      
+        for (int i = 0; i < 3; i++)
+        {
+            if (enableSpawn)
+            {
+                GameObject enemy = (GameObject)Instantiate(Enemy, new Vector3(row, 1.1f, 0f), Quaternion.identity);
+             
+                row += 50f;
+
+            }
+        }
+        ++round;
+        row += 50f;
+    }
+
+    void setRoundinfo() 
+    {
+        roundsinfo.text = "Round: " + round.ToString();
+    
+    
+    }
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 1, 3);
+        
+    }
+    void Update()
+    {
+
+    }
+}
